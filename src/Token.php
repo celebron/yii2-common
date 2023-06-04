a440e5ba8b43ef8aa20e9d8849b11725d783bb3e
celebron <celebron.ru@gmail.com>
2023-06-04 19:44:30 +0300

    Доделка класса

--- src/Token.php
+++ src/Token.php
@@ -9,20 +9,25 @@ use yii\helpers\Json;
 use yii\web\NotFoundHttpException;
 
 /**
- *
+ * Стандартные данные при получении Access token из OAuth2 сервера
  * @property-read bool $isExpires
  * @property-read int $expiresTime
  */
-class Token extends BaseObject
+class Token extends BaseObject implements \Stringable
 {
+    /** @var string - Токен доступа */
     public readonly string $accessToken;
+    /** @var int - Время существования токина в секундах  */
     public readonly int $expiresIn;
+    /** @var string|null - Токен для регенерации токена */
     public readonly ?string $refreshToken;
+    /** @var string|null - Тип токена, как правило, Bearer */
     public readonly ?string $tokenType;
-
+    /** @var array - остальные данные */
     public readonly array $data;
+    /** @var int - дата генерации в секундах (для вычисления Expire) */
     public int $generateTime = 0;
-
+    /** @var int - задержка, для регенерации токена (чтобы не пропустить) */
     public int $timeout = 3600 * 24;
 
     public function __construct (array $data, array $config = [])
@@ -37,34 +42,48 @@ class Token extends BaseObject
         parent::__construct($config);
     }
 
-
+    /**
+     * Проверка времени на просрок ExpireIn
+     * @return bool - Не просрочен/Просрочен
+     */
     public function getIsExpires (): bool
     {
         return time() > ($this->getExpiresTime() - $this->timeout);
     }
 
+    /**
+     * Вычисление времени действия токина относительно времени сервера
+     * @return int
+     */
     public function getExpiresTime():int
     {
         return $this->generateTime + $this->expiresIn;
     }
 
-    public function create (string $file):int|false
+    /**
+     * Создания файлы со значениями полученные OAuth2 сервера
+     * @param string $file - имя файла (можно использовать @)
+     * @return int|false
+     */
+    public function createFile (string $file):int|false
     {
         $file = \Yii::getAlias($file);
-        $json = Json::encode($this->toArray());
-        return file_put_contents($file, $json, LOCK_EX);
+        return file_put_contents($file, $this->__toString(), LOCK_EX);
     }
 
     /**
+     * Открытие файла со значениями
+     * @param string $file - имя файла (можно использовать @)
+     * @return static
      * @throws NotFoundHttpException
      */
-    public static function openFile (string $file, array $params = []): static
+    public static function openFile (string $file): static
     {
         $file = \Yii::getAlias($file);
         if (file_exists($file)) {
             \Yii::info("Open file {$file}.", static::class);
             $data = Json::decode(file_get_contents($file));
-            return new static($data, $params);
+            return new static($data);
         }
 
         throw new NotFoundHttpException("File '{$file}' not exists");
@@ -81,5 +100,10 @@ class Token extends BaseObject
         ];
 
         return ArrayHelper::merge($data, $this->data);
+    }
+
+    public function __toString ()
+    {
+        return Json::encode($this->toArray());
     }
 }
